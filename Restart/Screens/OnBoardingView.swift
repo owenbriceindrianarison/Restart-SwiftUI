ec9e967de70aa3bb723fd4961cfc63556fd5b3f2
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-06-01 01:07:00 +0200

    Basic Functionality

---
+++ Restart/Screens/OnBoardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnBoardingView.swift
+//  Restart
+//
+//  Created by Owen on 01/06/2022.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+   
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnBoardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingView()
+    }
+}
