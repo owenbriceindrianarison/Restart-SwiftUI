875fd05c2b0d9826059dd273d204f8152bc1ba8d
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-05-31 23:32:54 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Owen on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
