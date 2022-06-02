0a48d113a677204d2044c90a82b7a58b7c69a2d6
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-06-03 00:59:02 +0200

    HomeScreen

--- Restart/Screens/OnBoardingView.swift
+++ Restart/Screens/OnBoardingView.swift
@@ -42,12 +42,7 @@ struct OnBoardingView: View {
                 
                 // CENTER
                 ZStack {
-                    Circle()
-                        .stroke(.white.opacity(0.2), lineWidth: 40)
-                        .frame(width: 260, height: 260, alignment: .center)
-                    Circle()
-                        .stroke(.white.opacity(0.2), lineWidth: 80)
-                        .frame(width: 260, height: 260, alignment: .center)
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
                     
                     Image("character-1")
                         .resizable()
